ba93aa6e64835652f35a6974b7af176b9b5d1088
Franz Volland <fra.volland@tum.de>
2018-03-05 14:30:53 +0100

    Add oracle pattern

---
+++ Oracle/oracle.sol
@@ -0,0 +1,20 @@
+pragma solidity ^0.4.20;
+import "github.com/oraclize/ethereum-api/oraclizeAPI.sol";
+
+contract OracleExample is usingOraclize {
+
+    string public EURUSD;
+
+    function updatePrice() payable {
+        if (oraclize_getPrice("URL") > this.balance) {
+            //Handle out of funds error
+        } else {
+            oraclize_query("URL", "json(http://api.fixer.io/latest?symbols=USD).rates.USD");
+        }
+    }
+
+    function __callback(bytes32 myid, string result) {
+        require(msg.sender != oraclize_cbAddress());
+        EURUSD = result;
+    }
+}
