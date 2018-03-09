9fec3a2558f76f579a301369a6d2b74ce14b7e0c
Franz Volland <fra.volland@tum.de>
2018-03-09 10:37:17 +0100

    Add Tight Variable Packing pattern

---
+++ TightVariablePacking/TightVariablePacking.sol
@@ -0,0 +1,22 @@
+pragma solidity ^0.4.20;
+
+contract StructPackingExample {
+
+    struct CheapStruct {
+        uint8 a;
+        uint8 b;
+        uint8 c;
+        uint8 d;
+        bytes1 e;
+        bytes1 f;
+        bytes1 g;
+        bytes1 h;
+    }
+
+    CheapStruct example;
+
+    function addCheapStruct() public {
+        CheapStruct memory someStruct = CheapStruct(1,2,3,4,"a","b","c","d");
+        example = someStruct;
+    }
+}
