99e2edc1416a0ddc31e7ac496cd80912876b2307
Franz Volland <fra.volland@tum.de>
2018-03-09 10:22:50 +0100

    Add State Machine pattern

---
+++ StateMachine/StateMachine.sol
@@ -0,0 +1,55 @@
+pragma solidity ^0.4.20;
+
+contract StateMachine {
+
+    enum Stages {
+        AcceptingBlindBids,
+        RevealBids,
+        WinnerDetermined,
+        Finished
+    }
+
+    Stages public stage = Stages.AcceptingBlindBids;
+
+    uint public creationTime = now;
+
+    modifier atStage(Stages _stage) {
+        require(stage == _stage);
+        _;
+    }
+
+    modifier transitionAfter() {
+        _;
+        nextStage();
+    }
+
+    modifier timedTransitions() {
+        if (stage == Stages.AcceptingBlindBids && now >= creationTime + 6 days) {
+            nextStage();
+        }
+        if (stage == Stages.RevealBids && now >= creationTime + 10 days) {
+            nextStage();
+        }
+        _;
+    }
+
+    function bid() public payable timedTransitions atStage(Stages.AcceptingBlindBids) {
+        // Implement biding here
+    }
+
+    function reveal() public timedTransitions atStage(Stages.RevealBids) {
+        // Implement reveal of bids here
+    }
+
+    function claimGoods() public timedTransitions atStage(Stages.WinnerDetermined) transitionAfter {
+        // Implement handling of goods here
+    }
+
+    function cleanup() public atStage(Stages.Finished) {
+        // Implement cleanup of auction here
+    }
+
+    function nextStage() internal {
+        stage = Stages(uint(stage) + 1);
+    }
+}
