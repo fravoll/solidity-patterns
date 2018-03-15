dbf0405978c037477c2ade600ed7caede767763e
Franz Volland <fra.volland@tum.de>
2018-03-15 17:20:19 +0100

    Add Access Restriction pattern

---
+++ AccessRestriction/AccessRestriction.sol
@@ -0,0 +1,34 @@
+pragma solidity ^0.4.21;
+
+contract AccessRestriction {
+
+    address public owner = msg.sender;
+    uint public lastOwnerChange = now;
+
+    modifier onlyBy(address _account) {
+        require(msg.sender == _account);
+        _;
+    }
+
+    modifier onlyAfter(uint _time) {
+        require(now >= _time);
+        _;
+    }
+
+    modifier costs(uint _amount) {
+        require(msg.value >= _amount);
+        _;
+        if (msg.value > _amount) {
+            msg.sender.transfer(msg.value - _amount);
+        }
+    }
+
+    function changeOwner(address _newOwner) public onlyBy(owner) {
+        owner = _newOwner;
+    }
+
+    function buyContract() public payable onlyAfter(lastOwnerChange + 4 weeks) costs(1 ether) {
+        owner = msg.sender;
+        lastOwnerChange = now;
+    }
+}
