4ab3a97a97241c370524f518e04e59ed1b1b1552
fravoll <franz.volland@freenet.de>
2018-03-15 12:33:35 +0100

    Add spaces

--- StringEqualityComparison/StringEqualityComparison.sol
+++ StringEqualityComparison/StringEqualityComparison.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.4.20;
 contract StringEqualityComparison {
 
     function hashCompareWithLengthCheck(string a, string b) internal returns (bool) {
-        if(bytes(a).length != bytes(b).length) {
+        if (bytes(a).length != bytes(b).length) {
             return false;
         } else {
             return keccak256(a) == keccak256(b);
