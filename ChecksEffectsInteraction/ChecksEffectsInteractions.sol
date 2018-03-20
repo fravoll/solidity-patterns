d202207b6445203be2936d5f6c3e187a37c3124a
Franz Volland <fra.volland@tum.de>
2018-03-20 12:38:56 +0100

    Add Checks Effects Interactions pattern

---
+++ ChecksEffectsInteraction/ChecksEffectsInteractions.sol
@@ -0,0 +1,19 @@
+pragma solidity ^0.4.21;
+
+contract ChecksEffectsInteractions {
+
+    mapping(address => uint) balances;
+
+    function deposit() public payable {
+        balances[msg.sender] = msg.value;
+    }
+
+    function withdraw(uint amount) public {
+
+        require(balances[msg.sender] >= amount);
+
+        balances[msg.sender] -= amount;
+
+        msg.sender.transfer(amount);
+    }
+}
