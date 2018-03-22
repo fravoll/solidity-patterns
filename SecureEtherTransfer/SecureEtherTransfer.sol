ebc0375f43c1a47aad972ce3ec5c4aa3bd259b36
Franz Volland <fra.volland@tum.de>
2018-03-22 16:57:37 +0100

    Add secure ether transfer pattern

---
+++ SecureEtherTransfer/SecureEtherTransfer.sol
@@ -0,0 +1,25 @@
+pragma solidity ^0.4.21;
+
+contract EtherReceiver {
+
+    function () public payable {}
+}
+
+contract EtherSender {
+
+    EtherReceiver private receiverAdr = new EtherReceiver();
+
+    function sendEther(uint _amount) public payable {
+        if (!address(receiverAdr).send(_amount)) {
+            //handle failed send
+        }
+    }
+
+    function callValueEther(uint _amount) public payable {
+        require(address(receiverAdr).call.value(_amount).gas(21000)());
+    }
+
+    function transferEther(uint _amount) public payable {
+        address(receiverAdr).transfer(_amount);
+    }
+}
