fb970287ce82340310f5007dff9a75dc816cc387
Franz Volland <fra.volland@tum.de>
2018-03-27 16:52:15 +0200

    Add Emergency Stop pattern

---
+++ EmergencyStop/EmergencyStop.sol
@@ -0,0 +1,37 @@
+pragma solidity ^0.4.21;
+
+contract EmergencyStop {
+
+    bool isStopped = false;
+
+    modifier stoppedInEmergency {
+        require(!isStopped);
+        _;
+    }
+
+    modifier onlyWhenStopped {
+        require(isStopped);
+        _;
+    }
+
+    modifier onlyAuthorized {
+        // Check for authorization of msg.sender here
+        _;
+    }
+
+    function stopContract() public onlyAuthorized {
+        isStopped = true;
+    }
+
+    function resumeContract() public onlyAuthorized {
+        isStopped = false;
+    }
+
+    function deposit() public payable stoppedInEmergency {
+        // Deposit logic happening here
+    }
+
+    function emergencyWithdraw() public onlyWhenStopped {
+        // Emergency withdraw happening here
+    }
+}
