15ff103368050fb1114e8554019a1393135f5e2c
Franz Volland <franz.volland@freenet.de>
2018-04-19 11:53:58 +0200

    Update randomness for 0.4.22 and close bets at setSeed

--- Randomness/Randomness.sol
+++ Randomness/Randomness.sol
@@ -1,38 +1,38 @@
 // This code has not been professionally audited, therefore I cannot make any promises about
 // safety or correctness. Use at own risk.
 
-pragma solidity ^0.4.20;
+pragma solidity ^0.4.22;
 
 contract Randomness {
 
     bytes32 sealedSeed;
     bool seedSet = false;
-    bool betMade = false;
+    bool betsClosed = false;
     uint storedBlockNumber;
     address trustedParty = 0xdCad3a6d3569DF655070DEd06cb7A1b2Ccd1D3AF;
 
     function setSealedSeed(bytes32 _sealedSeed) public {
         require(!seedSet);
         require (msg.sender == trustedParty);
+        betsClosed = true;
         sealedSeed = _sealedSeed;
         storedBlockNumber = block.number + 1;
         seedSet = true;
     }
 
     function bet() public {
-        require(!betMade);
+        require(!betsClosed);
         // Make bets here
-        betMade = true;
     }
 
     function reveal(bytes32 _seed) public {
         require(seedSet);
-        require(betMade);
+        require(betsClosed);
         require(storedBlockNumber < block.number);
         require(keccak256(msg.sender, _seed) == sealedSeed);
-        uint random = uint(keccak256(_seed, block.blockhash(storedBlockNumber)));
+        uint random = uint(keccak256(_seed, blockhash(storedBlockNumber)));
         // Insert logic for usage of random number here;
         seedSet = false;
-        betMade = false;
+        betsClosed = false;
     }
 }
