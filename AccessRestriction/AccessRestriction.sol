2baf0b68ddcf8f7cc3b86ce94206df15c172346f
Param Choudhary <69947192+Paramchoudhary@users.noreply.github.com>
2022-01-18 21:29:25 +0530

    Update AccessRestriction.sol

--- AccessRestriction/AccessRestriction.sol
+++ AccessRestriction/AccessRestriction.sol
@@ -1,37 +1,33 @@
-// This code has not been professionally audited, therefore I cannot make any promises about
-// safety or correctness. Use at own risk.
 
-pragma solidity ^0.4.21;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
-contract AccessRestriction {
+contract AccessRestriction{
+    uint time_ = block.timestamp;
+ address payable public owner  = payable(msg.sender);
+ uint public lastOwnerChange = time_;
+ modifier onlyBy(address _account){
+     require(msg.sender == _account);
+     _;
+ }
+ modifier onlyAfter(uint _time){
+     require(block.timestamp >= _time);
+     _;
 
-    address public owner = msg.sender;
-    uint public lastOwnerChange = now;
+ }
+ modifier costs(uint _amount){
+     require(msg.value >= _amount);
+     _;
+     if(msg.value > _amount){
+   owner.transfer(msg.value - _amount);
+     }
+ }
+ function changeOwner(address payable _newOwner) public onlyBy(owner){
+     owner  = _newOwner;
+ }
+ function buycontract() public payable onlyAfter(lastOwnerChange + 4 weeks) {
+     owner = payable(msg.sender);
+     lastOwnerChange = block.timestamp;
+ }
 
-    modifier onlyBy(address _account) {
-        require(msg.sender == _account);
-        _;
-    }
-
-    modifier onlyAfter(uint _time) {
-        require(now >= _time);
-        _;
-    }
-
-    modifier costs(uint _amount) {
-        require(msg.value >= _amount);
-        _;
-        if (msg.value > _amount) {
-            msg.sender.transfer(msg.value - _amount);
-        }
-    }
-
-    function changeOwner(address _newOwner) public onlyBy(owner) {
-        owner = _newOwner;
-    }
-
-    function buyContract() public payable onlyAfter(lastOwnerChange + 4 weeks) costs(1 ether) {
-        owner = msg.sender;
-        lastOwnerChange = now;
-    }
 }
