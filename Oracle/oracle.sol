efab6b2f04737f84b74800c001fd30d1c7c76e23
Swaraj <swarajmali072004@gmail.com>
2023-08-13 04:13:07 +0530

    fixed the error in oracle.sol

--- Oracle/oracle.sol
+++ Oracle/oracle.sol
@@ -1,22 +1,22 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.5.0 < 0.9.0 ;
+
 // This code has not been professionally audited, therefore I cannot make any promises about
 // safety or correctness. Use at own risk.
-
-pragma solidity ^0.4.20;
-import "github.com/oraclize/ethereum-api/oraclizeAPI_0.4.sol";
-
-contract Oracle is usingOraclize {
+import "github.com/oraclize/ethereum-api/oraclizeAPI.sol";
+contract OracleExample is usingOraclize{
 
     string public EURUSD;
 
     function updatePrice() public payable {
-        if (oraclize_getPrice("URL") > this.balance) {
+        if (oraclize_getPrice("URL") > address(this).balance) {
             //Handle out of funds error
         } else {
-            oraclize_query("URL", "json(https://api.exchangeratesapi.io/latest?symbols=USD).rates.USD");
+            oraclize_query("URL", "json(http://api.fixer.io/latest?symbols=USD).rates.USD");
         }
     }
-
-    function __callback(bytes32 myid, string result) public {
+    
+    function __callback(bytes32  myid, string memory result) public {
         require(msg.sender == oraclize_cbAddress());
         EURUSD = result;
     }
